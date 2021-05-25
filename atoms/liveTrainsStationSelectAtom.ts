89b48a43c3a3014afcca50c7d12c5bf9b93e4f65
David Wheatley <hi@davwheat.dev>
2021-05-25 10:21:12 +0100

    Add arrival station atoms

--- atoms/liveTrainsStationSelectAtom.ts
+++ atoms/liveTrainsStationSelectAtom.ts
@@ -10,3 +10,13 @@ export const liveTrains_departureStationFilterAtom = atom({
   key: 'liveTrains_departureStationFilter',
   default: '',
 })
+
+export const liveTrains_arrivalStationAtom = atom<StationPair | null>({
+  key: 'liveTrains_arrivalStation',
+  default: null,
+})
+
+export const liveTrains_arrivalStationFilterAtom = atom({
+  key: 'liveTrains_arrivalStationFilter',
+  default: '',
+})
