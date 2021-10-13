aece67a854378e60f2138d3df0db3df667ee66b2
David Wheatley <hi@davwheat.dev>
2021-10-13 14:19:10 +0100

    chore: bump RN to 0.66

--- metro.config.js
+++ metro.config.js
@@ -6,5 +6,11 @@ config.transformer.minifierPath = 'metro-minify-terser'
 config.transformer.minifierConfig = {
   // Terser options...
 }
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+})
 
 module.exports = config
